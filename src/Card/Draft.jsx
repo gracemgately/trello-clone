426c600d4961fbb3828daeda33d5078718b14e40
gracemgately <gracemgately@gmail.com>
2022-01-10 17:14:44 -0500

    Add basic text validation on the cards

--- src/Card/Draft.jsx
+++ src/Card/Draft.jsx
@@ -1,20 +1,37 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+const validate = text => {
+    if (text.length > 500) throw 'Maximum card length is 500 characters';
+}
+
 const Draft = ({ cancel, confirm }) => {
     const [text, setText] = useState('Enter a title for this card');
+    const [validationError, setValidationError] = useState(null);
 
     const onChange = e => {
+        if (validationError) setValidationError(null);
         setText(e.target.value);
+    };
+
+    const validateDraftText = () => {
+        try {
+            validate(text);
+            confirm(text);
+        }
+        catch(e) {
+            setValidationError(e);
+        }
     };
 
     return (
         <div>
         <textarea className='card draft' data-testid='draft' defaultValue={text} onChange={onChange}/>
-        <button className='text-button confirm' onClick={() => confirm(text)}>
+        <button className='text-button confirm' onClick={validateDraftText}>
             Add a card</button>
         <button className='text-button' aria-label='Cancel' onClick={cancel}>
             X</button>
+        { validationError ? <p>{validationError}</p> : null }
         </div>
     );
 };
--- src/Card/Draft.test.jsx
+++ src/Card/Draft.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Draft from './Draft';
 
@@ -18,5 +18,30 @@ describe('Draft', () => {
     userEvent.type(screen.getByRole('textbox'), ' value');
 
     screen.getByText('Enter a title for this card value');
+  });
+
+  it('validates text when the user tries to create the card', async () => {
+    const textWith501Characters = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. ' +
+    'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus ' +
+    'et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ' +
+    'ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. ' +
+    'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, ' +
+    'imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus';
+
+    const props = { 
+      cancel: () => {},
+      confirm: () => {},
+    };
+    render(<Draft {...props}/>);
+
+    const draft = screen.queryByTestId('draft');
+    expect(draft).toBeInTheDocument();
+    fireEvent.change(screen.getByRole('textbox'), { target: { value: '' } });
+
+    userEvent.type(screen.getByRole('textbox'), textWith501Characters);
+
+    userEvent.click(screen.getByRole('button', { name: 'Add a card' }));
+    
+    await waitFor(() => screen.getByText('Maximum card length is 500 characters'));
   });
 });
